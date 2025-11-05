9b9a4884ea4c143ee495d9293ac9930af74ebdc6
Victor Seva <linuxmaniac@torreviejawireless.org>
2025-11-05 18:03:23 +0100

    settings: define env_prefix

--- pyaput/conf/settings.py
+++ pyaput/conf/settings.py
@@ -1,7 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="APUT_")
+
     project_name: str = "pyaput"
     debug: bool = False
     aptly_api_url: str = "https://aptly.home.arpa/api/"
